d461f308afbf3576857c359b4e6f7b9b1164f31c
Robert Howden <robert.howden@current-rms.com>
2020-04-22 10:24:31 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -2,8 +2,27 @@ function getIt() {
   $('p').on("click", () => alert('Hey!'));
 }
 
+function frameIt() {
+  $('p').on("click", () => alert('Hey!'));
+}
+
+function getIt() {
+  $('p').on("click", () => alert('Hey!'));
+}
+
+function pressIt() {
+  $('p').on("click", () => alert('Hey!'));
+}
+
+function submitIt() {
+  $("form").on("submit", () => alert('your form is going to be submitted now'));
+}
+
 $(document).ready(function(){
 
 getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
